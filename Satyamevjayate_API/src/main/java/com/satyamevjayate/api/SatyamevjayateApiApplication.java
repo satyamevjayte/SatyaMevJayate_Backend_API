b04ab059392966ad5f9bb5befcd2ccc15356022b
taild001 <DhirenKumar.Tailor@vcontractor.co.za>
2021-04-11 19:21:55 +0530

    Add two API

--- Satyamevjayate_API/src/main/java/com/satyamevjayate/api/SatyamevjayateApiApplication.java
+++ Satyamevjayate_API/src/main/java/com/satyamevjayate/api/SatyamevjayateApiApplication.java
@@ -2,8 +2,11 @@ package com.satyamevjayate.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+
 public class SatyamevjayateApiApplication {
 
 	public static void main(String[] args) {
